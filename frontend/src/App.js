1fb3684b045504130d14d91d95e3af3bc482ee87
zathazass <sathananthanit15@gmail.com>
2022-04-10 20:10:58 +0530

    Added frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1><span Style="color: dodgerblue;">Track</span> My Work</h1>
+        <p Style="font-size: 1rem;">Powered by ZassCodes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
