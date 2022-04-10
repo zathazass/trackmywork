05147ae40592f3c06c3157583d944e6f603dd059
zathazass <sathananthanit15@gmail.com>
2022-04-10 21:02:00 +0530

    removed base level warnings and errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1><span Style="color: dodgerblue;">Track</span> My Work</h1>
-        <p Style="font-size: 1rem;">Powered by ZassCodes</p>
+        <h1><span style={{ color: 'dodgerblue' }}>Track</span> My Work</h1>
+        <p style={{ fontSize: '1rem' }}>Powered by ZassCodes</p>
       </header>
     </div>
   );
