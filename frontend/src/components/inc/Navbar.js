32e8a898fc614a0edcd02aff39f0589d85523e10
zathazass <sathananthanit15@gmail.com>
2022-04-10 22:44:07 +0530

    added some pages for testing(works fine or not)

---
+++ frontend/src/components/inc/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<div className="container-fluid">
+				<Link to='/' className='navbar-brand'>TrackMyWork</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link to='/home' className='nav-link' aria-current="page">Home</Link>
+						</li>
+						<li className="nav-item">
+							<Link to='/about' className='nav-link'>About</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import TrackMyWork from './TrackMyWork';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +12,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TrackMyWork />
   </React.StrictMode>
 );
 
