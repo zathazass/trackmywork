64a8067cbe652987c7c8518158ab485c2a1b354a
zathazass <sathananthanit15@gmail.com>
2022-07-03 19:11:00 +0530

    added index page

--- trackmywork/urls.py
+++ trackmywork/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-def index(request):
-    return HttpResponse('<h1>You cannot use backend directly</h1>')
+def index_page(request):
+    return render(request, 'index.html')
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index_page, name='index_page'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('__reload__/', include('django_browser_reload.urls'))
